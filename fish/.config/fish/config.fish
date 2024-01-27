3871fb7f370306d1139978f2a8601096fe7d6698
TOMES-TT <4095651+TOMES-TT@users.noreply.github.com>
2024-01-26 21:51:48 -0600

    Initial commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # get colors from the current pywal colorscheme
+    cat ~/.cache/wal/sequences &
+
+    # aliases
+    alias a="ls"
+    alias sa="ls -a"
+    alias he="helix"
+    alias bs="helix ~/.config/bspwm/bspwmrc"
+    alias sx="helix ~/.config/sxhkd/sxhkdrc"
+    alias fcf="helix ~/.config/fish/config.fish"
+    alias xn="helix ~/.xinitrc"
+    alias ra="ranger"
+end
