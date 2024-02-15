845c34cdfe90205daf9966116b76e5151706ab40
TOMES-TT <4095651+TOMES-TT@users.noreply.github.com>
2024-02-14 21:30:02 -0600

    updated bspwm colors, added aliases for useful functions, updated hotkeys for window manipulation and swapping, and updated helix cursor shapes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,22 +4,31 @@ if status is-interactive
     # get colors from the current pywal colorscheme
     cat ~/.cache/wal/sequences &
 
+    #
     # aliases
+    #
 
     # ls
     alias a="ls"
     alias aa="ls -a"
     alias la="ls -lh"
+    alias ra="ls -R"
 
     # rm
     alias rm="trash"
 
+    #commands
     alias he="helix"
 
+    # config files
     alias bs="helix ~/.config/bspwm/bspwmrc"
     alias sx="helix ~/.config/sxhkd/sxhkdrc"
+    alias hec="helix ~/.config/helix/config.toml"
     alias fcf="helix ~/.config/fish/config.fish"
     alias xn="helix ~/.xinitrc"
-    alias ra="ranger"
+    alias rn="ranger"
     alias pbc="he ~/.config/polybar/config.ini"
+
+    #git management
+    alias github="stow $HOME/dotfiles/*/ && git add $HOME/dotfiles/* && git commit"
 end
