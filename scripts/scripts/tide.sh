a10b18f0a6ad537e88bf466d3c97000ebbbf8c31
TOMES-TT <4095651+TOMES-TT@users.noreply.github.com>
2024-03-15 12:33:23 -0500

    added wrapping in helix config; added languages to helix; added bookreport script; added monitor config to xinitrc; moved desktop-based commands to bspwmrc

--- scripts/scripts/tide.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env/bash
-
-set -l _tide_tmp_dir (command mktemp -d)
-curl https://codeload.github.com/ilancosman/tide/tar.gz/v6 | tar -xzC $_tide_tmp_dir
-command cp -R $_tide_tmp_dir/*/{completions,conf.d,functions} $__fish_config_dir
-fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
-
