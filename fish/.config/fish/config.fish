82c52618624cb2ea171c74d524d0f6e80bdcd2ff
TOMES-TT <4095651+TOMES-TT@users.noreply.github.com>
2024-10-04 20:40:36 -0500

    updated bspwm focus; made fish aliases work regardless of interactive status; added helix settings; added lf image previews

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,41 +4,43 @@ if status is-interactive
     # get colors from the current pywal colorscheme
     source ~/.cache/wal/colors.fish
 
-    #
-    # aliases
-    #
-
-    # ls
-    alias a="ls"
-    alias aa="ls -a"
-    alias la="ls -lh"
-    alias ra="ls -R"
-
-    # rm
-    alias tr="trash"
-
-    # commands
-    alias hx="helix"
-    alias nc="mpc update && ncmpcpp"
-    alias neo="neo -C $HOME/.config/neo/colors"
-
-    # vpn control
-    alias mvd="mullvad disconnect"
-    alias mvc="mullvad connect"
-    alias mvs="mullvad status"
-
-    # config files
-    alias bs="helix ~/.config/bspwm/bspwmrc"
-    alias sx="helix ~/.config/sxhkd/sxhkdrc"
-    alias hxc="helix ~/.config/helix/config.toml"
-    alias fcf="helix ~/.config/fish/config.fish"
-    alias xn="helix ~/.xinitrc"
-    alias rn="ranger"
-    alias pbc="helix ~/.config/polybar/config.ini"
-    alias ala="helix ~/.config/alacritty/alacritty.toml"
 
     #git management
     alias github="cd $HOME/dotfiles/; stow */ ; git add ./* ; git commit; git push"
 
     alias ct="setxkbmap -layout us -variant dvorak -option compose:menu ctrl:swapcaps"
 end
+
+#
+# aliases
+#
+
+# ls
+alias a="ls"
+alias aa="ls -a"
+alias la="ls -lh"
+alias ra="ls -R"
+
+# rm
+alias tr="trash"
+
+# commands
+alias he="helix"
+alias nc="mpc update && ncmpcpp"
+alias neo="neo -C $HOME/.config/neo/colors"
+alias lf="exec /usr/local/bin/lf-wrap"
+
+# vpn control
+alias mvd="mullvad disconnect"
+alias mvc="mullvad connect"
+alias mvs="mullvad status"
+
+# config files
+alias bs="helix ~/.config/bspwm/bspwmrc"
+alias sx="helix ~/.config/sxhkd/sxhkdrc"
+alias hec="helix ~/.config/helix/config.toml"
+alias fcf="helix ~/.config/fish/config.fish"
+alias xn="helix ~/.xinitrc"
+alias rn="ranger"
+alias pbc="helix ~/.config/polybar/config.ini"
+alias ala="helix ~/.config/alacritty/alacritty.toml"
